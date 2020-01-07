377e547e396fcecce88021f127eb00689107beee
Petr Mitin <petr09mitin@mail.ru>
2020-01-08 02:04:31 +0300

    Updates

---
+++ precache-manifest.8b0e4c933e9593ed2f80226900f40b19.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32b69f0a10a23f7d4dcd791a16aa0ff0",
+    "url": "/personalportfolio/index.html"
+  },
+  {
+    "revision": "2d53a1bca53c15afb64d",
+    "url": "/personalportfolio/static/css/main.551e99bf.chunk.css"
+  },
+  {
+    "revision": "73ce78720b42bf313bec",
+    "url": "/personalportfolio/static/js/2.c4f174a4.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/personalportfolio/static/js/2.c4f174a4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d53a1bca53c15afb64d",
+    "url": "/personalportfolio/static/js/main.f787394e.chunk.js"
+  },
+  {
+    "revision": "c004c450df3ae453884c",
+    "url": "/personalportfolio/static/js/runtime-main.647a42cc.js"
+  },
+  {
+    "revision": "902df4c501596c42571749648e081c71",
+    "url": "/personalportfolio/static/media/background_search_desktop.902df4c5.jpg"
+  },
+  {
+    "revision": "5d0905e66c87b168792e1cf920c613ee",
+    "url": "/personalportfolio/static/media/background_search_mobile.5d0905e6.jpg"
+  }
+]);
